ac69125c956d37810d1ab5cacba72276da1c8346
Kevin Tsang <mail@ktsang.com>
2025-08-15 13:15:25 +0100

    ready for demo, update toppan icon

--- src/api/application/application-config.ts
+++ src/api/application/application-config.ts
@@ -1,7 +1,7 @@
 import { countryLogo } from '@countryconfig/api/application/country-logo'
 
 export const applicationConfig = {
-  APPLICATION_NAME: 'Farajaland CRS',
+  APPLICATION_NAME: 'Toppan CRS',
   BIRTH: {
     REGISTRATION_TARGET: 30,
     LATE_REGISTRATION_TARGET: 365,
--- src/client-config.js
+++ src/client-config.js
@@ -27,8 +27,8 @@ window.config = {
   REGISTRATIONS_DASHBOARD_URL: '',
   // http://localhost:4444/public/dashboard/a17e9bc0-15a2-4bd1-92fa-ab0f346227ca#bordered=false&titled=false&refresh=300
   STATISTICS_DASHBOARD_URL: '',
-  FAMILY_TREE_URL: 'http://localhost:3888/showMap',
-  PERSON_SEARCH_API_URL: 'http://localhost:7070',
+  FAMILY_TREE_URL: 'http://localhost:3889/familyTree',
+  TOPPAN_SERVICE_URL: 'http://localhost:7070',
   DEBUG: true,
   FEATURES: {
     // The V2_EVENTS variable is passed down from src/index.ts:309
